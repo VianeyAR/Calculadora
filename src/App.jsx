db8b2d7832ffcc71e73c1eab0938b011c8587f39
Vianey <vianey@equipo3.com>
2023-03-10 12:49:28 -0600

    Sombras

--- src/App.jsx
+++ src/App.jsx
@@ -1,88 +1,134 @@
-
 import './App.css'
+
 function App() {
 
   return (
-    <div>
-      <h1>Calculator</h1>
+    <div className='app'>
+      <h1>Calculadora</h1>
       <hr />
-      <table style={{
-        width: '400px'
-        }}>
-        <tr >
-        <td colSpan={4}>Result</td>
+      <table style={{ width: '400px', margin: 'auto'}}>
         
-        </tr>
-        {/*Second row*/}
+         {/*FIRT ROW */}
         <tr>
-          <td ><button 
-            type='button'
-            className='btn btn-primary' >c</button></td>
-          <td ><button 
-            type='button'
-            className='btn btn-primary' >/</button></td>
-          <td ><button 
-            type='button'
-            className='btn btn-primary' >*</button></td>
-          <td ><button 
-            type='button'
-            className='btn btn-primary' >-</button></td>
+          <td colSpan={4} style={{
+            border: '1px solid black', textAlign: 'end'
+            }}>
+            <h2>0</h2>
+          </td>
         </tr>
 
-        <tr >
-          <td ><button 
-            type='button'
-            className='btn btn-primary' >7</button></td>
-          <td ><button 
-            type='button'
-            className='btn btn-primary' >8</button></td>
-          <td ><button 
-            type='button'
-            className='btn btn-primary' >9</button></td>
-          <td rowSpan={2}><button 
-            type='button'
-            className='btn btn-primary' >+</button></td>
+         {/*SECOND ROW */}
+        <tr>
+          <td>
+          <button 
+             tipe='button' 
+             className='btn btn-info w-75 mt-2'>C</button>
+          </td>
+          <td>
+          <button 
+             tipe='button' 
+             className='btn btn-info w-75 mt-2'>/</button>
+          </td>
+          <td>
+          <button 
+             tipe='button' 
+             className='btn btn-info w-75 mt-2'>*</button>
+          </td>
+          <td>
+          <button 
+             tipe='button' 
+             className='btn btn-info w-75 mt-2'>-</button>
+          </td>
         </tr>
 
-        <tr >
-          <td ><button 
-            type='button'
-            className='btn btn-primary' >4</button></td>
-          <td ><button 
-            type='button'
-            className='btn btn-primary' >5</button></td>
-          <td ><button 
-            type='button'
-            className='btn btn-primary' >6</button></td>
-          
+         {/*THIRD ROW */}
+        <tr>
+          <td>
+          <button 
+             tipe='button' 
+             className='btn btn-info w-75 mt-2'>7</button>
+          </td>
+          <td>
+          <button 
+             tipe='button' 
+             className='btn btn-info w-75 mt-2'>8</button>
+          </td>
+          <td>
+          <button 
+             tipe='button' 
+             className='btn btn-info w-75 mt-2'>9</button>
+          </td>
+          <td rowSpan={2}>
+          <button 
+             tipe='button' 
+             className='btn btn-info w-75 mt-2'
+             style={{height:"85px"}}>+</button>
+          </td>
         </tr>
 
-        <tr >
-          <td><button 
-            type='button'
-            className='btn btn-primary' >1</button></td>
-          <td><button 
-            type='button'
-            className='btn btn-primary' >2</button></td>
-          <td><button 
-            type='button'
-            className='btn btn-primary' >3</button></td>
-          <td rowSpan={2}><button 
-            type='button'
-            className='btn btn-primary' >=</button></td>
+         {/*FOURTH ROW */}
+        <tr>
+          <td>
+          <button 
+             tipe='button' 
+             className='btn btn-info w-75 mt-2'>4</button>
+          </td>
+          <td>
+          <button 
+             tipe='button' 
+             className='btn btn-info w-75 mt-2'>5</button>
+          </td>
+          <td>
+          <button 
+             tipe='button' 
+             className='btn btn-info w-75 mt-2'>6</button>
+          </td>
         </tr>
 
-        <tr >
-        <td colSpan={2}>0</td>
-         <td> <button 
-            type='button'
-            className='btn btn-primary'>
-            .</button></td>
-          
-          
+        {/*FIFTH ROW */}
+        <tr>
+          <td>
+            <button 
+             tipe='button' 
+             className='btn btn-info w-75 mt-2'>1</button> 
+          </td>
+          <td>
+          <button 
+             tipe='button' 
+             className='btn btn-info w-75 mt-2'>2</button>
+          </td>
+          <td>
+          <button 
+             tipe='button' 
+             className='btn btn-info w-75 mt-2'>3</button>
+          </td>
+          <td rowSpan={2}>
+          <button 
+             tipe='button' 
+             className='btn btn-info w-75 mt-2'
+             style={{height: "85px"}}>=</button>
+          </td>
         </tr>
 
-        
+        {/*SIXTH ROW */}
+        <tr>
+          <td>
+          <button 
+             tipe='button' 
+             className='btn btn-info mt-2 w-75'> DEL </button>
+          </td>
+          <td>
+          <button 
+             tipe='button' 
+             className='btn btn-info mt-2 w-75'>0</button>
+          </td>
+          <td>
+            <button 
+             tipe='button' 
+             className='btn btn-info w-75 mt-2'>.</button>
+          </td>
+        </tr>
+
       </table>
     </div>
   )
